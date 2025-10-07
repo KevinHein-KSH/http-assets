eb09cc43285186d6b60dd90ab1458576c09d451e
kevin <kaungsetthein@phinion.com>
2025-10-07 23:44:14 +0700

    Ch-2: DNS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import FetchIPAddress from './components/DNS/fetchIPAddress';
 import FetchData from './components/Fetch_API/fetchCall';
 
 export default function App() {
     return (
         <>
             <FetchData />
+            <FetchIPAddress />
         </>
     );
 }
