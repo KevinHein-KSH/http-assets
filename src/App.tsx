986015b213c7bb8603d584d59ed2ab8d0e4539fb
kevin <kshein.it@gmail.com>
2025-10-19 16:23:09 +0700

    Revert "Merge branch 'Ch-2-DNS' into Ch-3-URL"
    This reverts commit 78e4f6deec0eeb599dd4a8d1cfe7e056c4ab23a9, reversing
    changes made to 48e598c217f95bcdfb4d409f450327b3512c7414.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import FetchIPAddress from './components/Ch2:DNS/FetchIPAddress';
-import FetchData from './components/Ch1:Fetch_API/FetchCall';
+import FetchIPAddress from './components/DNS/fetchIPAddress';
+import FetchData from './components/Fetch_API/fetchCall';
+import UrlViewer from './components/URL/urlParts';
 
 export default function App() {
     return (
         <>
             <FetchData />
             <FetchIPAddress />
+            <UrlViewer />
         </>
     );
 }
