b7ba35c5aa20a1844e075c7340975a48a6289a96
kevin <kshein.it@gmail.com>
2025-10-19 15:51:21 +0700

    Revert "Merge branch 'main' into Ch-2-DNS"
    This reverts commit c850e0210ccbeae7564fa9e738a403343916cc57, reversing
    changes made to 53f2b4867bed4c209e4010a038e1d597969d193c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import FetchIPAddress from './components/DNS/FetchIPAddress';
 import FetchData from './components/Fetch_API/FetchCall';
-import UrlViewer from './components/URL | Async/URLParts';
 
 export default function App() {
     return (
         <>
             <FetchData />
             <FetchIPAddress />
-            <UrlViewer />
         </>
     );
 }
