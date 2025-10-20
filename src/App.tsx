0079eb69041fae8b337ac0a328fb6c93cfa719ec
kevin <kshein.it@gmail.com>
2025-10-20 18:54:00 +0700

    renamed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import FetchIPAddress from './components/Ch2:DNS/fetchIPAddress';
 import FetchData from './components/Ch1:Fetch_API/fetchCall';
-import UrlViewer from './components/URL/urlParts';
+import UrlViewer from './components/Ch3:URL/urlParts';
 
 export default function App() {
     return (
