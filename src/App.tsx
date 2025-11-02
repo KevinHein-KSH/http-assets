9ef161bc9dee8f327005d18ffcb32a00d31c8df5
kevin <kshein.it@gmail.com>
2025-11-02 17:38:51 +0700

    WIP: disable Template for now

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
             <FetchIPAddress />
             <UrlViewer />
             
-            <TemplateContainer />
+            {/* <TemplateContainer /> */}
         </>
     );
 }
