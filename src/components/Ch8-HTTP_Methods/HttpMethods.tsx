a59c1a817b412ec57aafbc96c532bb0d989b0c7e
kevin <kshein.it@gmail.com>
2025-12-18 22:34:58 +0700

    sudo to do

---
+++ src/components/Ch8-HTTP_Methods/HttpMethods.tsx
@@ -0,0 +1,13 @@
+// fetch call usinng URL and apiKey parameters
+// in header Get method
+// use cros as mode
+// add X-API-KEY in header
+
+// 1. Take URL and data as parameters
+// 2. call fetch 
+// 2.1 use Post method
+// 2.2 use cros as mode
+// 2.3 add X-API-KEY in header, with apiKey parameter
+// 2.4 add Content-Type application/json in header as its value
+// 2.5 add body as JSON.stringify(data)
+// 3. return response as json
